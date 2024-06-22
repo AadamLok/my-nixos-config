0039d9c37edbb0718c76f0e414450972f0064a06
Aadam <alokhandwala@umass.edu>
2024-06-22 10:30:40 -0400

    Fixed error with let..in in flake file

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 		};
 	};
 
-	outputs = { nixpkgs, home-manager, ... }: {
+	outputs = { nixpkgs, home-manager, ... }:
 		let 
 			system = "x86_64-linux";
 		in {
@@ -28,5 +28,4 @@
 				];
 			};
 		};
-	};
 }
