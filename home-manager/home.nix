8253e8ee58e1aebf0df21399d3a2066d5bd24638
Aadam <alokhandwala@umass.edu>
2024-06-22 11:15:17 -0400

    Restructured zsh config to be it's own file

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -8,50 +8,7 @@
 		];
 	};
 
-	programs.zsh = {
-		enable = true;
-		enableCompletion = true;
-		autosuggestion.enable = true;
-		syntaxHighlighting.enable = true;
-		shellAliases = {
-			ll = "ls -l";
-			la = "ls -la";
-			rebuild-switch = "sudo nixos-rebuild switch --flake ~/nixos";
-			rebuild-boot = "sudo nixos-rebuild boot --flake ~/nixos";
-			hm-switch = "home-manager switch --flake ~/nixos/#aadamlok";
-		};
-		
-		oh-my-zsh = {
-			enable = true;
-			theme = "robbyrussell";
-			plugins = [
-				"sudo" # Hit esc to add sudo to command
-				"systemadmin" # Many useful things
-				# Important once
-				# mkdir, tree
-				# clrz -> clear zombie process
-				# port_pro -> shows process with port
-				# dls -> ls only dirs
-				"vi-mode" # vi like feature on shell
-				"web-search" # google/youtube/stackoverflow ... will search and open browser
-				"copypath" # copypath will copy pwd
-				"copyfile" # copyfile ... will copy content of the file to your clipboard
-				"dirhistory" # Helps navigate dirs
-				# Alt + left => previous dir
-				# Alt + right => next dir
-				# Alt + up => parent dir
-				# Alt + down => first child dir
-				"history" # command history
-				# h => print history
-				# hl => pipe history to less
-				# hs => pipe history to grep
-				"python" # useful for python
-				# mkv [name] => makes virtual env
-				# vrun [name] => run [name] virtual env
-				# pygrep <text> => looks for <text> in *.py file recursively
-				# pyclean => deletes byte-code and cache files
-				# pyfind => finds all .py file recursively
-			];
-		};	
-	};
+	imports = [
+		./zsh.nix
+	];
 }
