1d069ea5c06e7d732aa2481dae0e49f24477e4eb
Aadam Lokhandwala <alokhandwala@umass.edu>
2024-06-28 19:21:07 -0400

    Made basic flake file

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+	description = "My System Configuration";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	};
+
+	outputs = { nixpkgs, ... }: {
+		let 
+			system = "x86_64-linux";
+		in {
+		
+			nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+				inherit system;
+				modules = [
+					./nixos/configuration.nix
+				];	
+			};
+		};
+	};
+}
