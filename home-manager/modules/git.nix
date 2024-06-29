0fd07070a786bfa4096d39600cc8deb1de72192c
Aadam Lokhandwala <alokhandwala@umass.edu>
2024-06-29 13:02:02 -0400

    Configured zsh and git in home-manager

---
+++ home-manager/modules/git.nix
@@ -0,0 +1,11 @@
+{config, ...}: {
+	programs.git = {
+		enable = true;
+		userName = "AadamLok";
+		userEmail = "alokhandwala@umass.edu";
+		#signing = {
+		#	signByDefault = true;
+		#	key = "79293CA28AD1069C";
+		#};
+	};
+}
