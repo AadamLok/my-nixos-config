17ceff1343605280b927d35f733d4a1317494fe2
Aadam Lokhandwala <alokhandwala@umass.edu>
2024-07-05 15:57:48 -0400

    Added animations to hyprland

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -38,15 +38,14 @@ in
 				10)
 			);
 			exec-once = ''${startupScript}/bin/start'';
-			genral = {
+			general = {
 				gaps_in = 5;
 				gaps_out = 20;
 				border_size = 2;
 				"col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
 				"col.inactive_border" = "rgba(595959aa)";
 				resize_on_border = true;
-				allow_tearing = false;
-				layout = dwindle;
+				layout = "dwindle";
 			};
 			decoration = {
 				rounding = 10;
@@ -57,11 +56,23 @@ in
 				shadow_render_power = 3;
 				"col.shadow" = "rgba(1a1a1aee)";
 				blur = {
-					enable = true;
+					enabled = true;
 					size = 3;
 					passes = 1;
 					vibrancy = 0.1696;
 				};
+			};
+			animations = {
+				enabled = true;
+				bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+				animation = [
+					"windows, 1, 7, myBezier"
+					"windowsOut, 1, 7, default, popin 80%"
+					"border, 1, 10, default"
+					"borderangle, 1, 8, default"
+					"fade, 1, 7, default"
+					"workspaces, 1, 6, default"
+				];
 			};
 		};
 	};
